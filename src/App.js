aa294650a94d9d3f640786c98360c13f3b2afb52
Zahra Dabirzadeh <Queenz@Zahras-MBP.fios-router.home>
2021-03-14 20:20:12 -0400

    added css styles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import React from 'react';
-import './App.css';
+import './index.css';
 
 const App = () => {
 return(
   <div className="calculator">
+    <div id="display"
+    className="display">
+      125
+    </div>
+    <button>1</button>
+    <button>2</button>
+    <button>3</button>
+    <button>4</button>
+    <button>1</button>
+    <button>2</button>
+    <button>3</button>
+    <button>4</button>
+    <button>1</button>
+    <button>2</button>
+    <button>3</button>
+    <button>4</button>
+    <button>1</button>
+    <button>2</button>
+    <button>3</button>
+    <button>4</button>
     <button>1</button>
     <button>2</button>
     <button>3</button>
     <button>4</button>
   </div>
+  
 )
 }
 
