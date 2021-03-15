39979468176457d609b9bc4fb307be1e096e79ac
Zahra Dabirzadeh <hizahrazadeh@gmail.com>
2021-03-14 20:39:08 -0400

    updated buttons

--- src/App.js
+++ src/App.js
@@ -27,8 +27,8 @@ return(
     <button>3</button>
     <button>4</button>
     <button>1</button>
-    <button>2</button>
-    <button>3</button>
+    <button className="big-v">2</button>
+    <button className="big-h">3</button>
     <button>4</button>
   </div>
   
