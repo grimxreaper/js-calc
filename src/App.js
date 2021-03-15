a07ef67dde63cabedb83f0b999908d30b27f8346
Zahra Dabirzadeh <hizahrazadeh@gmail.com>
2021-03-14 20:31:27 -0400

    updated css buttons

--- src/App.js
+++ src/App.js
@@ -11,15 +11,16 @@ return(
     </div>
     <button>1</button>
     <button>2</button>
-    <button>3</button>
-    <button>4</button>
+    <button className="dark-grey">3</button>
+    <button className="light-grey">4</button>
     <button>1</button>
     <button>2</button>
     <button>3</button>
     <button>4</button>
-    <button>1</button>
-    <button>2</button>
-    <button>3</button>
+
+    <button className="orange">1</button>
+    <button className="orange">2</button>
+    <button className="orange">3</button>
     <button>4</button>
     <button>1</button>
     <button>2</button>
