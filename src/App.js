71d9b290a3b980a263e7cf11d565ada16acdf661
Zahra <hizahrazadeh@gmail.com>
2021-03-16 13:47:05 -0400

    added map functions

--- src/App.js
+++ src/App.js
@@ -1,41 +1,59 @@
-import React from 'react';
-import './index.css';
+import React from "react";
+import "./index.css";
 
-const num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-const operations = ['=', '+', '-', '/', '*','ac'];
+const nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const operations = ["/", "*", "-", "+", "="];
 
 const App = () => {
-return(
-  <div className="calculator">
-    <div id="display"
-    className="display">
-      125
+  return (
+    <div className="calculator">
+      <div id="display" className="display">
+        125
+        <div className="nums-container">
+        {nums.map((num) => {
+            return (
+              <button className="dark-grey" key={num}>
+                {num}
+              </button>
+            );
+          })}
+        </div>
+        <div className="operations-container">
+          {operations.map((operation) => {
+            return (
+              <button className="orange" key={operation}>
+                {operation}
+              </button>
+            );
+          })}
+        </div>
+      </div>
     </div>
-    <button>1</button>
-    <button>2</button>
-    <button className="dark-grey">3</button>
-    <button className="light-grey">4</button>
-    <button>1</button>
-    <button>2</button>
-    <button>3</button>
-    <button>4</button>
+  );
+};
 
-    <button className="orange">1</button>
-    <button className="orange">2</button>
-    <button className="orange">3</button>
-    <button>4</button>
-    <button>1</button>
-    <button>2</button>
-    <button>3</button>
-    <button>4</button>
-    <button>1</button>
-    <button className="big-v">2</button>
-    <button className="big-h">3</button>
-    <button>4</button>
-  </div>
-  
-)
+{
+  /* <button>1</button>
+<button>2</button>
+<button className="dark-grey">3</button>
+<button className="light-grey">4</button>
+<button>1</button>
+<button>2</button>
+<button>3</button>
+<button>4</button>
+
+<button className="orange">1</button>
+<button className="orange">2</button>
+<button className="orange">3</button>
+<button>4</button>
+<button>1</button>
+<button>2</button>
+<button>3</button>
+<button>4</button>
+<button>1</button>
+<button className="big-v">2</button>
+<button className="big-h">3</button>
+<button>4</button> */
 }
-
 
 export default App;
