788cac56721aa3cf287b987d69c848746a80ab08
Zahra <hizahrazadeh@gmail.com>
2021-03-16 16:58:45 -0400

    ac and dot done

--- src/App.js
+++ src/App.js
@@ -7,34 +7,51 @@ const operations = ["/", "×", "-", "+", "="];
 class App extends React.Component {
   state = {
     lastPressed: undefined,
-    currentNumber: '0',
-    prevNumber: undefined
-  }
+    currentNumber: "0",
+    prevNumber: undefined,
+  };
 
   handleClick = (e) => {
     const { lastPressed, currentNumber, prevNumber } = this.state;
     const { innerText } = e.target; // we get the target which is the button, and the innerText gets what's inside the button!
-    
+
     if (!Number.isNaN(Number(innerText))) {
-      if (currentNumber === '0') {
+      if (currentNumber === "0") {
         this.setState({
-          currentNumber: innerText
-        })
+          currentNumber: innerText,
+        });
       } else {
+        this.setState({
+          currentNumber: currentNumber + innerText,
+        });
+      }
+    }
+    switch(innerText) {
+      case 'AC' : {
+        this.setState({
+          currentNumber : '0',
+          prevNumber: undefined
+        })
+        break;
+      }
+      case '.' : {
+        if (!currentNumber.includes('.'))
+          this.setState({
+            currentNumber: currentNumber + innerText
+          })
+          break;
+      }
+     
+
+
+      }
+
       this.setState({
-        currentNumber: currentNumber + innerText
-      })
-    }
-
-    }
-
-
-    this.setState({
-      lastPressed: innerText //so we're always setting what was last pressed
-    })
-
-   
+        lastPressed: innerText, //so we're always setting what was last pressed
+      });
   }
+
+
   render() {
     const { currentNumber } = this.state;
     return (
@@ -43,21 +60,32 @@ class App extends React.Component {
           {currentNumber}
         </div>
         <div className="nums-container">
-          <button className="big-h light-grey ac" onClick={this.handleClick}>AC</button>
+          <button className="big-h light-grey ac" onClick={this.handleClick}>
+            AC
+          </button>
           {nums.map((num) => {
             return (
-              <button className={`dark-grey ${num === 0 && "big-h"}`}
-              onClick={this.handleClick} key={num}>
+              <button
+                className={`dark-grey ${num === 0 && "big-h"}`}
+                onClick={this.handleClick}
+                key={num}
+              >
                 {num}
               </button>
             );
           })}
-          <button className="light-grey" onClick={this.handleClick}>.</button>
+          <button className="light-grey" onClick={this.handleClick}>
+            .
+          </button>
         </div>
         <div className="operations-container">
           {operations.map((operation) => {
             return (
-              <button className="orange" key={operation} onClick={this.handleClick}>
+              <button
+                className="orange"
+                key={operation}
+                onClick={this.handleClick}
+              >
                 {operation}
               </button>
             );
