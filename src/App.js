7c850a6931188e48a8d268970a9273942dd017cb
Zahra <hizahrazadeh@gmail.com>
2021-03-16 15:36:37 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -2,59 +2,48 @@ import React from "react";
 import "./index.css";
 
 const nums = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
-const operations = ["/", "*", "-", "+", "="];
+const operations = ["/", "×", "-", "+", "="];
 
-const App = () => {
-  return (
-    <div className="calculator">
-      <div id="display" className="display">
-        125
+class App extends React.Component {
+
+  handleClick = (e) => {
+    const { innerText } = e.target; // we get the target which is the button, and the innerText gets what's inside the button!
+    
+    switch(innerText) {
+      case 
+    }
+    alert(innerText);
+  }
+  render() {
+    return (
+      <div className="calculator">
+        <div id="display" className="display">
+          125
         </div>
         <div className="nums-container">
-        {nums.map((num) => {
+          <button className="big-h light-grey ac" onClick={this.handleClick}>AC</button>
+          {nums.map((num) => {
             return (
-              <button className={`dark-grey ${num == 0 && 'big-h'}`} key={num}>
+              <button className={`dark-grey ${num === 0 && "big-h"}`}
+              onClick={this.handleClick} key={num}>
                 {num}
               </button>
             );
           })}
+          <button className="light-grey" onClick={this.handleClick}>.</button>
         </div>
         <div className="operations-container">
           {operations.map((operation) => {
             return (
-              <button className="orange" key={operation}>
+              <button className="orange" key={operation} onClick={this.handleClick}>
                 {operation}
               </button>
             );
           })}
         </div>
       </div>
-
-  );
-};
-
-{
-  /* <button>1</button>
-<button>2</button>
-<button className="dark-grey">3</button>
-<button className="light-grey">4</button>
-<button>1</button>
-<button>2</button>
-<button>3</button>
-<button>4</button>
-
-<button className="orange">1</button>
-<button className="orange">2</button>
-<button className="orange">3</button>
-<button>4</button>
-<button>1</button>
-<button>2</button>
-<button>3</button>
-<button>4</button>
-<button>1</button>
-<button className="big-v">2</button>
-<button className="big-h">3</button>
-<button>4</button> */
+    );
+  }
 }
 
 export default App;
