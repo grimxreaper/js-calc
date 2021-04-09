0ca724c20cad3935b84e2c82da58155ca913ad68
Zahra <hizahrazadeh@gmail.com>
2021-04-09 11:43:46 -0400

    Using a method in a test file from App.js does not work for testing here

--- src/App.test.js
+++ src/App.test.js
@@ -32,14 +32,25 @@ it('displays open parens when clicked', () => {
 //2. press =
 //3. expect '56'
 
+const x = 8;
+const y = 7;
+const operation = "*"
 const expectedMultResult = 56;
 
-it('displays correct result of multiplying 8 by 7', () => {
-  const { getByTestId } = render(<App />);
-
-  fireEvent.click(getByTestId("8"), getByTestId("*"), getByTestId("7"), getByTestId("="))
-
-  expect(getByTestId("result")).toHaveTextContent("56")
+test('displays correct result of multiplying 8 by 7', () => {
+  expect(eval(`${x} ${operation} ${y}`)).toBe(56)
 })
+
+test('displays correct result of multiplying 8 by 7', () => {
+  expect(calculate(`${x} ${operation} ${y}`)).toBe(56)
+})
+
+// it('displays correct result of multiplying 8 by 7', () => {
+//   const { getByTestId } = render(<App />);
+
+//   fireEvent.click(getByTestId("8"), getByTestId("*"), getByTestId("7"), getByTestId("="))
+
+//   expect(getByTestId("result")).toHaveTextContent("56")
+// })
 
 //Testing events: division calculation 
