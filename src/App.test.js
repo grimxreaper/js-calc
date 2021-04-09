420305007e8ee2102a9fc0fcc5f0c02e3a0ef1b4
Zahra <hizahrazadeh@gmail.com>
2021-04-09 17:17:27 -0400

    Tests checking the operation of two numbers are passing

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import { render, screen, cleanup, fireEvent } from '@testing-library/react';
 import App from './App';
 import KeyPadComponent from './Components/KeyPadComponent';
 import ResultComponent from './Components/ResultComponent';
+import userEvent from '@testing-library/user-event';
 
 
 afterEach(cleanup)
@@ -17,8 +18,44 @@ it('should equal an empty string', () => {
   expect(getByTestId('result')).toHaveTextContent('')
 });
 
+//Testing user interaction
+test('division button displays when clicked', () => {
+  const { getByTestId } = render(<App />);
+
+  fireEvent.click(getByTestId("/"))
+
+  expect(getByTestId('result')).toHaveTextContent('/')
+
+  // userEvent.click(screen.getByRole('button', {name: /CE/i }))
+  // expect(screen.queryByText(children)).toBeInTheDocument();
+})
+
+test('result displays correct number and operation when used together', () => {
+  const { getByTestId } = render(<App />);
+
+  fireEvent.click(getByTestId(9))
+  fireEvent.click(getByTestId("/"))
+
+  expect(getByTestId('result')).toHaveTextContent('9/')
+})
+
+
+
+//'CE button deletes previous number or command'
+// test('CE button displays when clicked', () => {
+//   const { getByTestId } = render(<App />);
+//   const children = 'CE'
+
+//   fireEvent.click(getByTestId("CE"))
+
+//   expect(getByTestId('result')).toHaveTextContent('/')
+
+//   userEvent.click(screen.getByRole('button', {name: /CE/i }))
+//   expect(screen.queryByText(children)).toBeInTheDocument();
+// })
+
 //Testing events
-it('displays open parens when clicked', () => {
+it('state is updated when a button is clicked', () => {
   const { getByTestId } = render(<App />);
   
   fireEvent.click(getByTestId("("))
@@ -32,18 +69,45 @@ it('displays open parens when clicked', () => {
 //2. press =
 //3. expect '56'
 
+//Testing events: division calculation 
 const x = 8;
 const y = 7;
 const operation = "*"
-const expectedMultResult = 56;
 
 test('displays correct result of multiplying 8 by 7', () => {
   expect(eval(`${x} ${operation} ${y}`)).toBe(56)
 })
 
-test('displays correct result of multiplying 8 by 7', () => {
-  expect(calculate(`${x} ${operation} ${y}`)).toBe(56)
+describe('check the operation of 2 numbers', () => {
+  test('division of 9 by 2 must result in 4.5', () => {
+    const { getByTestId } = render(<App />);
+
+    fireEvent.click(getByTestId(9))
+    fireEvent.click(getByTestId("/"))
+    fireEvent.click(getByTestId(2))
+    fireEvent.click(getByTestId("="))
+    expect(getByTestId('result')).toHaveTextContent('4.5')
+  })
+
+  test('multiplication of 4 by 8 must result in 32', () => {
+    const { getByTestId } = render(<App />);
+
+    fireEvent.click(getByTestId(4))
+    fireEvent.click(getByTestId("*"))
+    fireEvent.click(getByTestId(8))
+    fireEvent.click(getByTestId("="))
+    expect(getByTestId('result')).toHaveTextContent('32')
+  })
 })
+
+
+//const calculate = require('./App')
+// test('displays correct result of multiplying 8 by 7', () => {
+//   const { getByTestId } = render(<App />);
+
+
+//   expect(calculate(`${x} ${operation} ${y}`)).toBe(56)
+// })
 
 // it('displays correct result of multiplying 8 by 7', () => {
 //   const { getByTestId } = render(<App />);
@@ -53,4 +117,5 @@ test('displays correct result of multiplying 8 by 7', () => {
 //   expect(getByTestId("result")).toHaveTextContent("56")
 // })
 
-//Testing events: division calculation 
+
+//Testing keyboard events
