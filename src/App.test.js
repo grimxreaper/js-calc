6893d051ca7ac0f81affc12b32fb38763ca72d13
Zahra <hizahrazadeh@gmail.com>
2021-04-09 20:38:06 -0400

    multiplication and addition with parens

--- src/App.test.js
+++ src/App.test.js
@@ -157,7 +157,23 @@ describe("check the operation of 3 numbers", () => {
     expect(getByTestId("result")).toHaveTextContent("165")
   });
 
+  test("multiplication and addition with parens", () => {
+    const { getByTestId } = render(<App />);
+
+    fireEvent.click(getByTestId("("));
+    fireEvent.click(getByTestId(7));
+    fireEvent.click(getByTestId("+"));
+    fireEvent.click(getByTestId(2));
+    fireEvent.click(getByTestId(")"));
+    fireEvent.click(getByTestId("*"));
+    fireEvent.click(getByTestId(2));
+    fireEvent.click(getByTestId("="));
+
+    expect(getByTestId("result")).toHaveTextContent("18")
+  });
+
 });
+
 
 describe('error message displaying at correct times', () => {
   test("multiplying 3 by 7 then adding 8 and hitting CE must equal error", () => {
