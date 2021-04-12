a81a91f87e711f7439c1dd0da30d7ca79313a172
Zahra <hizahrazadeh@gmail.com>
2021-04-12 12:46:19 -0400

    changes before updating again

--- src/App.js
+++ src/App.js
@@ -1,112 +1,99 @@
-import React from 'react';
-import './App.css';
-import ResultComponent from './Components/ResultComponent';
+import React from "react";
+import "./App.css";
+import ResultComponent from "./Components/ResultComponent";
 import KeyPadComponent from "./Components/KeyPadComponent";
 
 const operationKeys = ["/", "*", "-", "+"];
 const nums = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
 
 class App extends React.Component {
-  constructor(props){
-    super(props)
+  state = {
+    result: "",
+    counter: 0,
+    done: false,
+    operate: undefined,
+    originalLastNum: undefined,
+  };
 
-    this.state = {
-      result : "",
-      counter : 0,
-      operation : undefined,
-      originalLastNum : undefined,
-    }
-  }
-
-
-  
-  
   onClick = (button) => {
-    if (button === '='){
-      this.setState ({
-        counter: this.state.counter + 1
-        //originalLastNum: this.state.result 4 + 4
-        //4 + 4 
-        //slice to take out any keypads that are not an operation take what's left and set it as operation
-        //4 + 44 + 4444
-      })
-      this.calculate()
-    
+    if (button.type === "key") {
+      this.setState({ originalLastNum: button.key });
+    } else {
+      if (
+        button.key !== "CE" &&
+        button.key !== "=" &&
+        button.key !== "(" &&
+        button.key !== "AC" &&
+        button.key !== ")"
+      ) {
+        this.setState({ operate: button.key });
+      }
     }
-    else if (button === 'AC'){
-      this.reset()
+    if (button.key === "=") {
+      this.calculate();
+    } else if (button.key === "AC") {
+      this.reset();
+    } else if (button.key === "CE") {
+      this.backspace();
+    } else {
+      // this.setState({
+      //   result: this.state.result + button.key,
+      //   originalLastNum: button.key,
+      // });
     }
-    else if (button === 'CE'){
-      this.backspace()
-    }
-    else {
-      this.setState({
-        result: this.state.result + button,
-        originalLastNum: button
-      })
-
-      console.log('here is the originalLastNum:', this.state.originalLastNum)
-    }
-  }
+  };
 
   calculate = () => {
-    let currentResult = "";
-    let product = "";
-
-    if (this.state.result === '--') {
-      currentResult = this.state.result.replace('--', '+')
+    const { result, done, operate, originalLastNum } = this.state;
+    let lastKey = result.split(operate);
+    if (lastKey[1]) { //check if string is not alone
+    this.setState({ originalLastNum: lastKey[lastKey.length -1] });
     }
-    else {
-      currentResult = this.state.result
-    }
-
-    try {
-      // if (this.state.counter === 0) {
-        product = (eval(currentResult) || "") + ""
-        this.setState({
-          result: product
-        })
-    
-
-      // else {
-      //   product = this.state.result + this.state.operation + originalLastEqNum
-      //1.find a way to store all three of those
-      //   this.setState({
-      //     result: product
-      //   })
+    // console.log("🚀 ~ file: App.js ~ line 55 ~ App ~ originalLastNum", originalLastNum)
+    // console.log("🚀 ~ file: App.js ~ line 55 ~ App ~ result", result)
+    // console.log("🚀 ~ file: App.js ~ line 55 ~ App ~ operate", operate)
+    // if (done) {
+    //   let resultPrev = result + operate + originalLastNum;
+    //   let checkResult = (eval(resultPrev) || "") + "";
+    //   this.setState({ result: checkResult });
+    // } else {
+      // let currentResult = "";
+      // if (result === "--") {
+      //   console.log("🚀 ~ file: App.js ~ line 62 ~ App ~ result", result)
+      //   currentResult = result.replace("--", "+");
+      // } else {
+      //   currentResult = result;
       // }
-    }
-
-    catch (e){
       this.setState({
-        result: "error"
-      })
-    }
-  }
+        // done: true,
+        result: (eval(result + operate + originalLastNum) || "") + "",
+      });
+    // }
+  };
 
   reset = () => {
     this.setState({
-      result: ""
-    })
-  }
+      result: "",
+      done: false,
+    });
+  };
 
   backspace = () => {
     this.setState({
-      result: this.state.result.slice(0, -1)
-    })
-  }
+      result: this.state.result.slice(0, -1),
+    });
+  };
 
-  render(){
+  render() {
     return (
-      <div className="container" >
+      <div className="container">
         <h1>Pocket Js Calculator</h1>
         <div className="calculator-body">
-          <ResultComponent result = {this.state.result}/>
-          <KeyPadComponent onClick = {this.onClick}/>
+          <ResultComponent result={this.state.result} />
+          <KeyPadComponent onClick={this.onClick} />
         </div>
       </div>
-    )
+    );
   }
-
 }
 export default App;
--- src/Components/KeyPadComponent.js
+++ src/Components/KeyPadComponent.js
@@ -1,33 +1,49 @@
 import React from "react";
 
-const keypads = ["(", "CE", ")", "AC", "/", "*", "-", "+", 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, "=", "."]
+const keypads = [
+  { type: "operation", key: "(" },
+  { type: "operation", key: "CE" },
+  { type: "operation", key: ")" },
+  { type: "operation", key: "AC" },
+  { type: "operation", key: "/" },
+  { type: "operation", key: "*" },
+  { type: "operation", key: "-" },
+  { type: "operation", key: "+" },
+  { type: "key", key: 9 },
+  { type: "key", key: 8 },
+  { type: "key", key: 7 },
+  { type: "key", key: 6 },
+  { type: "key", key: 5 },
+  { type: "key", key: 4 },
+  { type: "key", key: 3 },
+  { type: "key", key: 2 },
+  { type: "key", key: 1 },
+  { type: "key", key: 0 },
+  { type: "operation", key: "=" },
+  { type: "operation", key: "." },
+];
 const operationKeys = ["/", "*", "-", "+"];
 const nums = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
 
-
-
 class KeyPadComponent extends React.Component {
+  render() {
+    return (
+      <div className="button">
+        {keypads.map((keypad) => {
+          return (
+            <button
+              className="keypad"
+              key={keypad.key}
+              name={keypad.key}
+              data-testid={keypad.key}
+              onClick={(e) => this.props.onClick(keypad)}
+            >
+              {keypad.key}
+            </button>
+          );
+        })}
 
-
-    render() {
-        return (
-            <div className="button">
-                {keypads.map((keypad) => {
-                    return (
-                        <button
-                        className="keypad"
-                        key={keypad}
-                        name={keypad}
-                        data-testid={keypad}
-                        onClick={e => this.props.onClick(e.target.name)}
-                        >
-                            {keypad}
-                        </button>
-                    )
-
-                })}
-
-                {/* <button className="keypad" name="(" onClick={e => this.props.onClick(e.target.name)}>(</button>
+        {/* <button className="keypad" name="(" onClick={e => this.props.onClick(e.target.name)}>(</button>
                 <button className="keypad" name="CE" onClick={e => this.props.onClick(e.target.name)}>CE</button>
                 <button name=")" onClick={e => this.props.onClick(e.target.name)}>)</button>
                 <button  name="AC" onClick={e => this.props.onClick(e.target.name)}>AC</button><br/>
@@ -54,11 +70,9 @@ class KeyPadComponent extends React.Component {
                 <button name="0" onClick={e => this.props.onClick(e.target.name)}>0</button>
                 <button name="=" onClick={e => this.props.onClick(e.target.name)}>=</button>
                 <button name="/" onClick={e => this.props.onClick(e.target.name)}>÷</button><br/> */}
-            </div>
-        );
-    }
-
+      </div>
+    );
+  }
 }
-
 
 export default KeyPadComponent;
